3802306ed32d5afaf684367901010a52cca09944
bopajdowski <bopajdowski@gmail.com>
2021-01-30 10:04:17 +0100

    debug false dla glownego settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2vy(36#i29$h#y&j6rdl2t@5t-*k_nu7do7ixp!o0j4!9i$#-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
