600696542671eeef286db31f5d1eb0fed77cd00f
bopajdowski <bopajdowski@gmail.com>
2021-01-30 11:29:56 +0100

    czesc administracyjna + dodatki

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+
+# alternatywnie
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = [
+        'question',
+        'choice_text',
+        'votes',
+    ]
+
+    search_fields = [
+        'question__question_text',
+        'choice_text',
+    ]
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
